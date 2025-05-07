022d3b3a1df21472a14e294f8669308849e79747
cynthier <65384913+cynthier@users.noreply.github.com>
2025-05-07 23:33:56 +0800

    Create color_code.R

---
+++ color_code.R
@@ -0,0 +1,20 @@
+
+colors <- c(
+    "E13.5" = "#a9d0f5", 
+    "E15.5" = "#e58b29",  
+    "E13.5_ref" = "gray",
+    
+    "Control" = "#04a29d", 
+    "Vcl cKO" = "#b2012e", 
+    
+    "BP" = "#6b853e",
+    "Neuroblast" = "#fdcee6",  
+    "BranchA" = "#bca9f5", 
+    
+    "E13.5_Neuroblast" = "#E41A1C",  
+    "E15.5_Neuroblast" = "#FFED6F",  
+    "E13.5_BranchA" = "#4DAF4A",  
+    "E15.5_BranchA" = "#F5A9D0",
+    "E13.5_BP" = "#819ff7",
+    "E15.5_BP" = "#FAAC58"
+   )
