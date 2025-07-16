1828563dd3e97aa119d63243250244636ca53e1c
cynthier <65384913+cynthier@users.noreply.github.com>
2025-07-16 16:11:23 +0800

    Update and rename Covert.Format to Covert.Format.R

--- packs/Covert.Format.R
+++ packs/Covert.Format.R
@@ -1,3 +1,4 @@
+############# read Loom file
 library(loomR)
 library(SeuratDisk)
 
@@ -5,10 +6,11 @@ data <- Connect("/home/rawdata/Public/scRNA//GSM4504449_E18_10X_18_065.loom", mo
 data.ser <- as.Seurat(data)
 
 
+########### convert h5ad to seurat object
 library(Seurat)
 library(SeuratData)
 library(SeuratDisk)
 
 library(SeuratDisk)
-Convert("/home/lfliuhku/rawdata/Public/scRNA/final_fetal_object_cellxgene.h5ad", dest = "h5seurat", overwrite = F)
+Convert("/home/rawdata/Public/scRNA/final_fetal_object_cellxgene.h5ad", dest = "h5seurat", overwrite = F)
 obj <- LoadH5Seurat("final_fetal_object_cellxgene.h5seurat")
