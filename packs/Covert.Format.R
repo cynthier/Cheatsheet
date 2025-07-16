d56e44117b73771749fa565855192495b35c61b4
cynthier <65384913+cynthier@users.noreply.github.com>
2025-07-16 22:14:52 +0800

    Update Covert.Format.R

--- packs/Covert.Format.R
+++ packs/Covert.Format.R
@@ -11,6 +11,10 @@ library(Seurat)
 library(SeuratData)
 library(SeuratDisk)
 
-library(SeuratDisk)
+# library(rhdf5) ###### have a look on the data
+# h5f <- H5Fopen("/home//rawdata/Public/scRNA/human_fetal_gut/ENS_log_counts.h5seurat")
+# # h5ls(h5f, recursive = TRUE)
+# H5Fclose(h5f)library(SeuratDisk)
+
 Convert("/home/rawdata/Public/scRNA/final_fetal_object_cellxgene.h5ad", dest = "h5seurat", overwrite = F)
 obj <- LoadH5Seurat("final_fetal_object_cellxgene.h5seurat")
