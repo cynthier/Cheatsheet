fddf36afe0c1871f83d8cf1d1c4c011f6d69d23e
cynthier <65384913+cynthier@users.noreply.github.com>
2025-12-08 13:06:20 +0800

    Update colors.R

--- plot/colors.R
+++ plot/colors.R
@@ -1,117 +1,117 @@
 
-getcolors <- function(){ 
+getcolors = function(){ 
 
-    
-    colors <- c(
-         "neuronal progenitor" = "#1f78b4", 
-    "neuroblast" = "#fb9a99",
-    "neurons" = "#ff7f00",
-    "Doublet" = "#BE81F7",
-    "Singlet" = "#aaaaaa",
-        
-    "transient stage" = "#ceecf5",
-    "committed stage" = "#68b8d0",
-    
-    "Mutant"="#f82401", 
-    "Vcl cKO"="#f82401", 
-    "Control" = "#0f7dbd",
-        
-    "hENCC" = "#ace9fb", 
-    "Day9" = "#57a6bd", 
-    "Day20" = "#476d78",
-    
-    "Vagal NCC" = "#385450",
-    "hENCC_2" = "#8dd3c7", 
-    "hENCC_1" = "#385450", 
-    # "hNP_day9" = "#bebada", 
-    # "hNP_day20" = "#fed976", 
-    # "HDAC1_KD_Day9" = "#766eb1", 
-    # "HDAC1_KD_Day20" = "#fd8d3c", 
-    # "HDAC1_2_KD_Day20" = "#b10026", 
-        
-    "hNP_day9"= "#bebada", 
-    "hNP_day20"="#fcd90c", 
-    "HDAC1_KD_Day9"= "#4000FF", 
-    "HDAC1_KD_Day20"="#f75716", 
-    "HDAC1_2_KD_Day20"="#8a1518",
-    
-    "ctrl_ENCC" = "#F5A9E1", 
-    "ctrl_N_D20" = "#9A2EFE", 
-    "ctrl_N_D40" = "#00BFFF", 
-    "ctrl_iPSC" = "#23715d", 
-    
-    "IMR_ENCC" = "#21963e", 
-    "IMR_N_Diff_D20" = "#9A2EFE", 
-    "IMR_N_Diff_D40" = "#F5A9A9", 
-    "IMR_iPSC" = "#00FF80", 
-        
-     "0" = "#8ea9f8",  # Blue
-      "1" = "#1F77B4",  # Blue
-      "2" = "#FF7F0E",  # Orange
-      "3" = "#2CA02C",  # Green
-      "4" = "#D62728",  # Red
-      "5" = "#9467BD",  # Purple
-      "6" = "#8C564B",  # Brown
-      "7" = "#E377C2",  # Pink
-      "8" = "#7F7F7F",  # Gray
-      "9" = "#BCBD22",  # Olive
-      "10" = "#17BECF", # Cyan
-      "11" = "#FFBB78", # Light Orange
-      "12" = "#98DF8A", # Light Green
-      "13" = "#FF9896",  
 
-        
-     "C0" = "#8ea9f8",  # Blue
-      "C1" = "#1F77B4",  # Blue
-      "C2" = "#FF7F0E",  # Orange
-      "C3" = "#2CA02C",  # Green
-      "C4" = "#D62728",  # Red
-      "C5" = "#9467BD",  # Purple
-      "C6" = "#8C564B",  # Brown
-      "C7" = "#E377C2",  # Pink
-      "C8" = "#7F7F7F",  # Gray
-      "C9" = "#BCBD22",  # Olive
-      "C10" = "#17BECF", # Cyan
-      "C11" = "#FFBB78", # Light Orange
-      "C12" = "#98DF8A", # Light Green
-      "C13" = "#FF9896",  
+	colors = c(
+	"neuronal progenitor" = "#1f78b4", 
+	"neuroblast" = "#fb9a99",
+	"neurons" = "#ff7f00",
+	"Doublet" = "#BE81F7",
+	"Singlet" = "#aaaaaa",
 
-    "cycling neuronal progenitor 1"='#1f77b4',
-    "cycling neuronal progenitor 2"='#aec7e8',
-    
-    "neuronal progenitor 1"='#ff7f0e', 
-    "neuronal progenitor 2"='#FFDE59',
-    "neuronal progenitor 4"='#a0a156',
-    "neuronal progenitor 3"='#e1b829',
-    
-     "neuroblast 3"="#98df8a",
-    "neuroblast 1"='#34634c',
-    "neuroblast 2"="#3e9621",
+	"transient stage" = "#ceecf5",
+	"committed stage" = "#68b8d0",
 
-    "ENC (SST+)"='#ff9896',
-    "IPAN/IN"='#8c564b',
-    "IN?"='#c49c94',
-    "ENC (GAD2+)"='#e377c2',
-    # "ENC (PHOX2B+_KCNJ6+)"='#f7b6d2',
-    "ENC (PHOX2B+_KCNJ6+)"='#D20103',
-    # "ENC (TH+_PHOX2B+)"='#7f7f7f',
-    # "ENC (TH+_PHOX2B+)"='#060270',
-    "ENC (NOVA1_high)"='#c7c7c7',
-    
-    "BP" = "#6b853e", 
-    "BPearly" = "#94c243", 
-    "BPlate" = "#607838", 
-    "GP" = "#feb462", 
-    "GPlate" = "#FFDE59",
-    "NPearly" = "#fdcee6", 
-    "NPlate" = "#ff2f9a", 
-    "ENMFB" = "#d9d9d9",
-   "Neuroblast" = "#fdcee6",
-    "Branch A" = "#bca9f5", 
-    "Neuroblast" = "#E41A1C",   
-    "Branch B" = "#f180ba",
-    "GPearly" = "#D7DF01",
-    "GPlate" = "#fbb469"
-    )
-    return(colors)
+	"Mutant"="#f82401", 
+	"Vcl cKO"="#f82401", 
+	"Control" = "#0f7dbd",
+
+	"hENCC" = "#ace9fb", 
+	"Day9" = "#57a6bd", 
+	"Day20" = "#476d78",
+
+	"Vagal NCC" = "#385450",
+	"hENCC_2" = "#8dd3c7", 
+	"hENCC_1" = "#385450", 
+	# "hNP_day9" = "#bebada", 
+	# "hNP_day20" = "#fed976", 
+	# "HDAC1_KD_Day9" = "#766eb1", 
+	# "HDAC1_KD_Day20" = "#fd8d3c", 
+	# "HDAC1_2_KD_Day20" = "#b10026", 
+
+	"hNP_day9"= "#bebada", 
+	"hNP_day20"="#fcd90c", 
+	"HDAC1_KD_Day9"= "#4000FF", 
+	"HDAC1_KD_Day20"="#f75716", 
+	"HDAC1_2_KD_Day20"="#8a1518",
+
+	"ctrl_ENCC" = "#F5A9E1", 
+	"ctrl_N_D20" = "#9A2EFE", 
+	"ctrl_N_D40" = "#00BFFF", 
+	"ctrl_iPSC" = "#23715d", 
+
+	"IMR_ENCC" = "#21963e", 
+	"IMR_N_Diff_D20" = "#9A2EFE", 
+	"IMR_N_Diff_D40" = "#F5A9A9", 
+	"IMR_iPSC" = "#00FF80", 
+
+	"0" = "#8ea9f8",  # Blue
+	"1" = "#1F77B4",  # Blue
+	"2" = "#FF7F0E",  # Orange
+	"3" = "#2CA02C",  # Green
+	"4" = "#D62728",  # Red
+	"5" = "#9467BD",  # Purple
+	"6" = "#8C564B",  # Brown
+	"7" = "#E377C2",  # Pink
+	"8" = "#7F7F7F",  # Gray
+	"9" = "#BCBD22",  # Olive
+	"10" = "#17BECF", # Cyan
+	"11" = "#FFBB78", # Light Orange
+	"12" = "#98DF8A", # Light Green
+	"13" = "#FF9896",  
+
+
+	"C0" = "#8ea9f8",  # Blue
+	"C1" = "#1F77B4",  # Blue
+	"C2" = "#FF7F0E",  # Orange
+	"C3" = "#2CA02C",  # Green
+	"C4" = "#D62728",  # Red
+	"C5" = "#9467BD",  # Purple
+	"C6" = "#8C564B",  # Brown
+	"C7" = "#E377C2",  # Pink
+	"C8" = "#7F7F7F",  # Gray
+	"C9" = "#BCBD22",  # Olive
+	"C10" = "#17BECF", # Cyan
+	"C11" = "#FFBB78", # Light Orange
+	"C12" = "#98DF8A", # Light Green
+	"C13" = "#FF9896",  
+
+	"cycling neuronal progenitor 1"='#1f77b4',
+	"cycling neuronal progenitor 2"='#aec7e8',
+
+	"neuronal progenitor 1"='#ff7f0e', 
+	"neuronal progenitor 2"='#FFDE59',
+	"neuronal progenitor 4"='#a0a156',
+	"neuronal progenitor 3"='#e1b829',
+
+	"neuroblast 3"="#98df8a",
+	"neuroblast 1"='#34634c',
+	"neuroblast 2"="#3e9621",
+
+	"ENC (SST+)"='#ff9896',
+	"IPAN/IN"='#8c564b',
+	"IN?"='#c49c94',
+	"ENC (GAD2+)"='#e377c2',
+	# "ENC (PHOX2B+_KCNJ6+)"='#f7b6d2',
+	"ENC (PHOX2B+_KCNJ6+)"='#D20103',
+	# "ENC (TH+_PHOX2B+)"='#7f7f7f',
+	# "ENC (TH+_PHOX2B+)"='#060270',
+	"ENC (NOVA1_high)"='#c7c7c7',
+
+	"BP" = "#6b853e", 
+	"BPearly" = "#94c243", 
+	"BPlate" = "#607838", 
+	"GP" = "#feb462", 
+	"GPlate" = "#FFDE59",
+	"NPearly" = "#fdcee6", 
+	"NPlate" = "#ff2f9a", 
+	"ENMFB" = "#d9d9d9",
+	"Neuroblast" = "#fdcee6",
+	"Branch A" = "#bca9f5", 
+	"Neuroblast" = "#E41A1C",   
+	"Branch B" = "#f180ba",
+	"GPearly" = "#D7DF01",
+	"GPlate" = "#fbb469"
+	)
+	return(colors)
 }
